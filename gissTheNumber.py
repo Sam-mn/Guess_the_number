2acd7e1e53f7adf735e1869d4b61b414fd9f9564
samer munawwar <samer.mnr@gmail.com>
2021-09-09 21:28:28 +0200

    added the high score file and made som changes

--- gissTheNumber.py
+++ gissTheNumber.py
@@ -2,8 +2,8 @@ import random
 import sys
 
 user_name = input('Type your name please: ')
-max_num = 10
 min_num = 0
+max_num = 30
 random_num = random.randint(min_num, max_num) 
 play = True
 guess= 0
@@ -14,6 +14,19 @@ def print_the_result (user_name: str, times: int):
     fh = open(f'files/{user_name}_result.txt', 'w')
     fh.write(f'yaaaay, GOD JOB {user_name} you did it after {times} tries!.')
     fh.close()
+# read the file and get the high score
+def get_high_score():
+    high_score= 0
+    high_score_file= open('high_score.txt','r')
+    high_score = int(high_score_file.read())
+    return high_score
+
+# write the new score to the file
+def save_high_score(score: int):
+    # Write to the file
+    high_score_file = open("high_score.txt", "w")
+    high_score_file.write(str(score))
+    high_score_file.close()
 
 while play:
     while guess != random_num:
@@ -37,6 +50,9 @@ while play:
         # if the user guess the right number.  
         else:
             print(f'yaaaay, GOD JOB {user_name} you did it after {times} times.')
+            high_score = get_high_score()
+            if high_score > times:
+                save_high_score(times)
         # create a file function.
             print_the_result(user_name,times )
             play_again = input(f'{user_name} do yo want to play again?')
@@ -48,5 +64,3 @@ while play:
                 guess= 0
                 random_num = random.randint(min_num, max_num) 
                 play= True
-                
-

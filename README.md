d84135c42eef1cdc48465e5a33adb1b0cf2a087c
samer munawwar <samer.mnr@gmail.com>
2021-09-14 23:38:22 +0200

    made some improvement

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
     - Before starting the game you can chose the minimum and the max numbers to guess between.
     - After you start the game you should add your name.
     - If you want to stop the game just add 'q'.
+    - When the app asked about playing again you can answer with 'y' or 'n'.
     - You can not chose a higher or lower number than the max and the min numbers.
     - When you guess the right number, you will be asked to play again and if you answer with 'y' you will be able to play again and if you answer with 'n' you wil end the game.
--- gissTheNumber.py
+++ gissTheNumber.py
@@ -14,12 +14,13 @@ def print_the_result (user_name: str, times: int):
     fh = open(f'files/{user_name}_result.txt', 'w')
     fh.write(f'yaaaay, GOD JOB {user_name} you did it after {times} tries!.')
     fh.close()
-    
+
 # read the file and get the high score
 def get_high_score():
     high_score= 0
     high_score_file= open('high_score.txt','r')
     high_score = int(high_score_file.read())
+    '''Return the high score to use it for check'''
     return high_score
 
 # write the new score to the file
@@ -29,39 +30,46 @@ def save_high_score(score: int):
     high_score_file.write(str(score))
     high_score_file.close()
 
-while play:
-    while guess != random_num:
-        guess= (input(f'Hello {user_name} Guess a number between {min_num} and {max_num}: '))
-        stop=str(guess)
-        # to stop the game.
-        if guess == 'Q' or guess == 'q': 
-            play= False
-            break
-        # check the numbers.
-        elif int(guess) > max_num:
-            print(f'Sorry, but the max number is {max_num}')
-        elif int(guess) < min_num:
-            print(f'Sorry, but the min number is {min_num}')
-        elif int(guess) > random_num:
-            print('Sorry, guess again TOO high. :)')
-            times += 1
-        elif int(guess) < random_num:
-            print('Sorry, guess again TOO low. :)')
-            times += 1
-        # if the user guess the right number.  
-        else:
-            print(f'yaaaay, GOD JOB {user_name} you did it after {times} times.')
-            high_score = get_high_score()
-            if high_score > times:
-                save_high_score(times)
-        # create a file function.
-            print_the_result(user_name,times )
-            play_again = input(f'{user_name} do yo want to play again?')
-            if play_again == "N" or play_again == "n":
+while play and __name__ == "__main__":
+    try:
+        while guess != random_num:
+            guess= (input(f'Hello {user_name} Guess a number between {min_num} and {max_num}: '))
+            stop=str(guess)
+            # to stop the game.
+            if guess == 'Q' or guess == 'q': 
                 play= False
                 break
-            if play_again == 'Y' or play_again == 'y':
-                times= 0
-                guess= 0
-                random_num = random.randint(min_num, max_num) 
-                play= True
+            # check the numbers.
+            elif int(guess) > max_num:
+                print(f'Sorry, but the max number is {max_num}')
+            elif int(guess) < min_num:
+                print(f'Sorry, but the min number is {min_num}')
+            elif int(guess) > random_num:
+                print('Sorry, guess again TOO high. :)')
+                times += 1
+            elif int(guess) < random_num:
+                print('Sorry, guess again TOO low. :)')
+                times += 1
+            # if the user guess the right number.  
+            else:
+                print(f'yaaaay, GOD JOB {user_name} you did it after {times} times.')
+                high_score = get_high_score()
+                if high_score > times:
+                    save_high_score(times)
+            # create a file function.
+                print_the_result(user_name,times )
+                play_again = input(f'{user_name} do yo want to play again?')
+                if play_again.lower() == "n":
+                    play= False
+                    break
+                elif play_again.lower() == 'y':
+                    times= 0
+                    guess= 0
+                    random_num = random.randint(min_num, max_num) 
+                    play= True
+                elif play_again != 'y' or play_again != 'n':
+                    print('You are just allowed to answer with y or n.')
+                    play_again = input(f'{user_name} do yo want to play again?')
+    except:
+         '''if the user answer whit non numeric answer'''
+         print("You are just allowed to add numbers here, Thanks :)")            
